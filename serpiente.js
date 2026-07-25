0bcfecd16f519e7b203daa648b9db68de4f87f65
01BARRETO1 <karbar_1990@hotmail.com>
2026-07-25 04:57:26 -0500

    Cabeza de serpiente color y lineas de cada cuandrito

--- serpiente.js
+++ serpiente.js
@@ -1,25 +1,230 @@
 
-    // 1. Capturamos el canvas y su contexto de dibujo
-    const canvas = document.getElementById("canvasJuego");
-    const ctx = canvas.getContext("2d");
+// 1. Capturamos el canvas y su contexto de dibujo
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
+const TAMANIO_CELDA = 25;
+
+/* 
+/////Taller serpiente parte 2/////
+//CREACIÓN DE LA SERPIENTE
+//PASO 1 .- Crear un arreglo:
+//PASO 2 Agregar objetos de ejemplo con coordenadas
+ */
+
+/* const serpiente = [{x:5,y:4},
+  {x:10,y:10},
+  {x:8,y:0}
+]; */
+
+ //✅ EJERCICIO 1 Una serpiente horizontal.
+/* 
+const serpiente = [{x:0,y:0},
+  {x:1,y:0},
+  {x:2,y:0}
+];
+ */
+
+  //✅ EJERCICIO 2 Ahora esta serpiente 
+
+ /* 
+  const serpiente = [{x:8,y:9},
+  {x:9,y:9},
+  {x:10,y:9},
+  {x:10,y:10}
+];
+ */
+
+//✅ EJERCICIO 3 .- Una serpiente de 5 cuadros subiendo pegada al borde izquierdo
+
+const serpiente = [{x:0,y:9},
+  {x:0,y:10},
+  {x:0,y:11},
+  {x:0,y:12},
+  {x:0,y:13}
+];
 
 
-    
 
-    // Primera pintura del juego al cargar la página
-    dibujarTodo();
 
-    // =========================
-    // FUNCIONES DE DIBUJO
-    // =========================
+// Primera pintura del juego al cargar la página
+dibujarTodo();
 
-    function limpiarCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+// =========================
+// FUNCIONES DE DIBUJO
+// =========================
+
+function limpiarCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function dibujarTodo() {
+  //limpiarCanvas();
+
+  dibujarTablero();
+
+  /* 
+  pintarParte(5, 1);
+  pintarParte(0, 3);
+  //✅ PRUEBA 1 
+  pintarParte(5,5);
+  //✅ PRUEBA 2
+  pintarParte(10,2);
+  //✅ PRUEBA 3 Pintar un cuadrado pegado al borde inferior del canvas.
+  pintarParte(9,19);
+  //✅ PRUEBA 4 Pintar un cuadrado pegado al borde derecho del canvas. 
+  pintarParte(19,9);
+  //✅ PRUEBA 5 Pintar un cuadrado pegado al borde izquierdo del canva
+  pintarParte(0,9);
+  //✅ PRUEBA 6 Pintar un cuadrado en cualquier esquina del canvas. 
+  pintarParte(19,0);
+   */
+
+  pintarSerpiente()
+
+
+
+}
+///////////////
+//DIBUJAR UNA CUADRÍCULA 
+//Construir el tablero del juego usando líneas verticales y horizontales. 
+//Crear una constante:
+//Esta constante almacenará el tamaño del ancho y alto de cada cuadrado de la cuadrícula
+
+//Crear la función: 
+/* function dibujarTablero() {
+  //Dentro de de la función dibujarTablero, vamos a pintar una línea de prueba dentro del canvas.
+  ctx.strokeStyle = "blue"; //coloca un color de línea, similar a fillStyle
+  ctx.beginPath()//se invoca siempre para iniciar un trazo
+  ctx.moveTo(250, 250)//posición inicial de la figura, colocar cualquier valor, punto inicial 
+  ctx.lineTo(250, 0)//dibuja una línea desde la ultima posicion del, punto final 
+  //graficador, en este caso lo que puso en moveTo, hasta la posicion que
+  //recibe como parámetro, colocar cualquier valor
+  ctx.stroke()//dibuja la línea
+  //Invocar dibujarTablero() desde dibujarTodo() y probar el resultado
+  ctx.strokeStyle = "red";   // línea vertical al lado del centro  de arriba 
+  ctx.beginPath();
+  ctx.moveTo(250, 250);      // punto inicial en el centro(posicion eje x, posicion eje y) en el centro
+  ctx.lineTo(260, 0);        // sube hasta el borde superior al tope valor = 0 (, o) cero es el tope 10 es más abajo (260 al lado del centro arriba derecha)
+  ctx.stroke();
+
+  ctx.strokeStyle = "yellow";  // línea horizontal
+  ctx.beginPath();
+  //x=500 pixeles a la derecha
+  ctx.moveTo(500, 10);       // punto inicial borde superior derecho, (,0) si fuersa cero coincide con la linea verde y= 0 pixeles hacia abajo por eso 10 baja, 250 a la mitad derecha
+  ctx.lineTo(250, 250);      // va hacia centro el tope seria 500 , disparo el laser a la mitad al centro
+  ctx.stroke();
+
+  ctx.strokeStyle = "green"; // línea diagonal
+  ctx.beginPath();
+  ctx.moveTo(0, 500);        // esquina inferior izquierda
+  ctx.lineTo(500, 0);        // esquina superior derecha le digo <al borde derecho arriba-->500 y a la esquina de arriba ---> 0 o sea 10 es mas abajito y 250 el centro derecho
+  ctx.stroke();
+
+  //Ejemplo linea desde el centro hacia abajo 
+
+  ctx.strokeStyle = "white" //color, primero color
+
+  ctx.beginPath(); //se invoca siempre para iniciar un trazo
+
+  ctx.moveTo(250, 250); //Se inicia el punto, nace el punto 
+  //X = 250 → 250 píxeles hacia la derecha.
+  //y=500 → 500 píxeles hacia abajo. de arriba hacia abajo o sea 490 no topa el borde de abajo
+  ctx.lineTo(250, 500);//dibuja la linea 250 derecha o sea cero --> 0 serír la esquina abajo izquierda, y 250 que es a la derecha al centro o sea 500 seria a la derecha del borde final 
+
+  //Y por último .stroke() --> dibuja la linea
+
+  ctx.stroke();
+
+  for (let i = 25; i < canvas.height; i+=25) {
+
+    ctx.strokeStyle = "#00FF00"; // línea diagonal
+    ctx.beginPath();
+    ctx.moveTo(i,0);     
+    ctx.lineTo(i, 500);
+    ctx.stroke();
+
+  }
+  
+} */
+
+//PASO 9 
+//crear un for para pintar las líneas verticales.
+
+function dibujarTablero() {
+  for (let i = TAMANIO_CELDA; i < canvas.height; i += TAMANIO_CELDA) {
+
+    ctx.strokeStyle = "#00FF00"; 
+    ctx.beginPath();
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, canvas.width);
+    ctx.stroke();
+
+  }
+  //PASO 10
+  //Crear un segundo for para pintar las líneas horizontales
+  for (let i = TAMANIO_CELDA; i < canvas.width; i += TAMANIO_CELDA) {
+
+    ctx.strokeStyle = "#00FF00"; 
+    ctx.beginPath();
+    ctx.moveTo(0, i);
+    ctx.lineTo(canvas.height, i);
+    ctx.stroke();
+
+  }
+
+
+
+}
+
+//TALLER SERPIENTE PARTE 2
+//
+function pintarParte(lineaX, lineaY) {
+  /* • Pintar un cuadrado  
+  • Usar el tamaño de TAMANIO_CELDA  
+  • Calcular la posición real dentro del canvas   */
+  lineaX = lineaX * TAMANIO_CELDA;
+  let x = lineaX;
+  lineaY = lineaY * TAMANIO_CELDA;
+  let y = lineaY;
+  //ctx.fillStyle = "#27D3F5";
+  ctx.fillRect(x, y, TAMANIO_CELDA, TAMANIO_CELDA);
+  //Configurar color del borde y dibujar borde: 
+  //ctx.strokeStyle="#080896"
+  ctx.strokeRect(x, y, TAMANIO_CELDA, TAMANIO_CELDA);
+
+
+}
+
+//CREACIÓN DE LA SERPIENTE
+//PASO 1 .- Crear un arreglo:
+//PASO 2 Agregar objetos de ejemplo con coordenadas
+  //ESTÁ EN LA PARTE DE ARRIBA, DADO QUE ES LLAMADA AL PRICIPIO PARA LEERSE EN CASCADA DEBE ESTAR INICIALIZADA ARRIBA
+//PASO 3 
+//Crear la función: pintarSerpiente()
+function pintarSerpiente(){
+  
+  for(let i=0; i<serpiente.length; i++){
+    let cuerpo=serpiente[i];
+    if(i===0){
+      // Primer cuadro (cabeza)
+      ctx.fillStyle="#801f6f";
+      ctx.strokeStyle="#F5F30C"
+    }else{
+      // Resto del cuerpo
+      ctx.fillStyle = "#27D3F5";
+      ctx.strokeStyle="#080896"
     }
+    pintarParte(cuerpo.x,cuerpo.y);
+  }
+}
+  /* PASO 5 
+  En dibujarTodo(): 
+  • Eliminar llamadas individuales de pintarParte()  
+  • Invocar únicamente:  
+  pintarSerpiente() */
 
-    function dibujarTodo() {
-      limpiarCanvas();
-    }
+//EJERCICIOS DE MODIFICACIÓN
+//Modificar las coordenadas del arreglo para mostrar:
 
 
 
